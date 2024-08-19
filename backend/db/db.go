e87ac61ac3501b3ab5f4dd913b7952705d91b73c
Chris <125088905+wyattchris@users.noreply.github.com>
2024-08-19 13:32:23 -0700

    comments

--- backend/db/db.go
+++ backend/db/db.go
@@ -10,15 +10,18 @@ import (
 )
 
 func ConnectSupabaseDB() {
+	// check config
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Could not load config: %v", err)
 	}
 
+	// attempt connection
 	conn, err := pgx.Connect(context.Background(), cfg.DatabaseURL)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
+	// close for now since we're not using it
 	defer conn.Close(context.Background())
 
 	fmt.Println("Successfully connected to the database!")
--- backend/db/db_test.go
+++ backend/db/db_test.go
@@ -6,10 +6,6 @@ import (
 
 func TestDBConnection(t *testing.T) {
 	t.Run("TestConnectSupabaseDB", func(t *testing.T) {
-		// This will internally attempt to connect to the database and print success or log fatal errors.
 		ConnectSupabaseDB()
-
-		// If ConnectSupabaseDB runs without triggering log.Fatalf, the test will pass.
-		// Since log.Fatalf exits the program, any failure will automatically fail the test.
 	})
 }
