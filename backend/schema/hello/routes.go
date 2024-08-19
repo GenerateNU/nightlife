97b8501f839f45fbbf373fb5eac3136a94c56f60
Chris <125088905+wyattchris@users.noreply.github.com>
2024-08-19 12:36:04 -0700

    transactions (stub file)

--- backend/schema/hello/routes.go
+++ backend/schema/hello/routes.go
@@ -4,17 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-
 func HelloGroup(v1 *gin.RouterGroup) *gin.RouterGroup {
-    helloGroup := v1.Group("hello_world")
-    {
-        helloGroup.GET("", getHelloWorld)
-    }
-    return helloGroup
+	helloGroup := v1.Group("hello_world")
+	{
+		helloGroup.GET("", getHelloWorld)
+	}
+	return helloGroup
 }
 
-
-func getHelloWorld(c* gin.Context) {
+func getHelloWorld(c *gin.Context) {
 	c.String(200, "Hello World")
 }
