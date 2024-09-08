70f1dbb53c70f8b80ec0c3195915969690a99d28
Chris <125088905+wyattchris@users.noreply.github.com>
2024-09-08 16:15:48 -0400

    fix db connection

--- backend/internal/app/nightlife/pkg/config/config.go
+++ backend/internal/app/nightlife/pkg/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -14,17 +15,19 @@ type Config struct {
 	DatabaseURL string
 }
 
-func LoadConfig() (*Config, error) {
-	// get the relative path to the nightlife root dir from repo root
-	rootDir, err := filepath.Abs("../../") //TODO: Fix env loading
-	if err != nil {
-		log.Fatalf("Failed to get the root directory: %v", err)
-		return nil, err
+// loads configuration from the specified .env file path
+func LoadConfig(envPath string) (*Config, error) {
+	// if no custom env path, use the default relative path to root directory
+	if envPath == "" {
+		var err error
+		envPath, err = filepath.Abs("../../../../../../.env")
+		if err != nil {
+			return nil, fmt.Errorf("failed to get the root directory: %v", err)
+		}
 	}
 
-	// load .env from the root directory
-	envPath := filepath.Join(rootDir, ".env")
-	err = godotenv.Load(envPath)
+	// load .env from the specified directory
+	err := godotenv.Load(envPath)
 	if err != nil {
 		log.Fatalf("Error loading .env file from %s", envPath)
 		return nil, err
@@ -33,19 +36,19 @@ func LoadConfig() (*Config, error) {
 	supabaseURL := os.Getenv("SUPABASE_URL")
 	if supabaseURL == "" {
 		log.Fatalf("SUPABASE_URL is not set in .env")
-		return nil, err
+		return nil, fmt.Errorf("SUPABASE_URL is not set in .env")
 	}
 
 	supabaseKey := os.Getenv("SUPABASE_ANON_KEY")
 	if supabaseKey == "" {
 		log.Fatalf("SUPABASE_ANON_KEY is not set in .env")
-		return nil, err
+		return nil, fmt.Errorf("SUPABASE_ANON_KEY is not set in .env")
 	}
 
 	databaseURL := os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
 		log.Fatalf("DATABASE_URL is not set in .env")
-		return nil, err
+		return nil, fmt.Errorf("DATABASE_URL is not set in .env")
 	}
 
 	config := &Config{
--- backend/internal/app/nightlife/pkg/db/db_test.go
+++ backend/internal/app/nightlife/pkg/db/db_test.go
@@ -1,11 +1,11 @@
 package db
 
 import (
-// "testing"
+	"testing"
 )
 
-//func TestDBConnection(t *testing.T) {
-//	t.Run("TestConnectSupabaseDB", func(t *testing.T) {
-//		ConnectSupabaseDB()
-//	})
-//}
+func TestDBConnection(t *testing.T) {
+	t.Run("TestConnectSupabaseDB", func(t *testing.T) {
+		ConnectSupabaseDB()
+	})
+}
