5e95acbddd63388494b88ee74df82117a33c50b3
Cam Plume <116120547+CamPlume1@users.noreply.github.com>
2024-09-09 20:13:51 -0400

    Project structure refactor

--- backend/internal/app/nightlife/main.go
+++ backend/internal/app/nightlife/main.go
@@ -3,31 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 
 	"github.com/GenerateNU/nightlife/internal/app/nightlife/pkg/db"
-	"github.com/GenerateNU/nightlife/internal/app/nightlife/pkg/schema/hello"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	// test the database connection
 	db.ConnectSupabaseDB()
 
+	//Create App
 	app := fiber.New()
 
-	// add CORS middleware
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",                                      // Allows all origins
-		AllowMethods: "GET, POST, PUT, PATCH, DELETE, OPTIONS", // Allows specific HTTP methods
-		AllowHeaders: "Origin, Content-Type, Authorization",    // Allows specific headers
-	}))
+
+	//Initialize Middleware
+	middleware.UseMiddleware(&app)
 
 	// Hello Group
-	hello.RouteHelloGroup(app)
+	router.InitializeRoutes(&app)
 
-	fmt.Println(rand.Int()) // Unsafe random
-
+	//Run app
 	log.Fatal(app.Listen(":8080"))
 }
---
+++ backend/internal/app/nightlife/pkg/router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/GenerateNU/nightlife/internal/app/nightlife/pkg/schema/hello"
+)
+
+
+func InitializeRoutes(app *fiber.App){
+
+	// Add each route group here
+	hello.RouteHelloGroup(app)
+}
