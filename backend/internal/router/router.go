2c527b40319754b7bc28f7c799bb50ead35aad15
Ben Petrillo <benpetrillo.bp@gmail.com>
2024-09-24 10:27:52 -0400

    feat(routes.go): add auth route group

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/GenerateNU/nightlife/internal/config"
+	auth "github.com/GenerateNU/nightlife/internal/schema/auth/routes"
 	hello "github.com/GenerateNU/nightlife/internal/schema/hello/routes"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,5 +11,6 @@ func InitializeRoutes(app *fiber.App, cfg *config.Config) {
 
 	// Add each route group here
 	hello.RouteHelloGroup(app, cfg)
+	auth.RouteAuthGroup(app, cfg)
 
 }
