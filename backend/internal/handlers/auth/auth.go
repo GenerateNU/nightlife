89e7a852824d7f7fed64f208b8f9a7991027471c
Ben Petrillo <benpetrillo.bp@gmail.com>
2024-09-27 09:42:41 -0400

    feat(auth/login): update POST auth/login to new backend structure

---
+++ backend/internal/handlers/auth/auth.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"github.com/GenerateNU/nightlife/internal/auth"
+	"github.com/GenerateNU/nightlife/internal/config"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+)
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (s *Service) Login(c *fiber.Ctx) error {
+
+	var loginData LoginRequest
+
+	if err := c.BodyParser(&loginData); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to parse request body",
+		})
+	}
+
+	email := loginData.Email
+	password := loginData.Password
+
+	conf, err := config.LoadConfig()
+
+	println(email)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to load config",
+		})
+	}
+
+	authToken, err := auth.GetAuthToken(&conf.Supabase, email, password)
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Failed to authenticate user",
+		})
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"message":  "Logged In",
+		"email":    email,
+		"password": password,
+		"token":    authToken,
+	})
+}
