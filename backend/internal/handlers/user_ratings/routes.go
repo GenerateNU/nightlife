e3df2703961ec286cba79b03962d9cbe3c7869e6
Cam Plume <116120547+CamPlume1@users.noreply.github.com>
2024-09-28 11:23:29 -0400

    adding auth middleware

--- backend/internal/handlers/user_ratings/routes.go
+++ backend/internal/handlers/user_ratings/routes.go
@@ -15,6 +15,7 @@
 package userratings
 
 import (
+	"github.com/GenerateNU/nightlife/internal/auth"
 	"github.com/GenerateNU/nightlife/internal/types"
 	"github.com/gofiber/fiber/v2"
 )
@@ -25,7 +26,7 @@ func Routes(app *fiber.App, params types.Params) {
 	service := newService(params.Store)
 
 	//create a grouping
-	protected := app.Group("/userratings")
+	protected := app.Group("/userratings").Use(auth.Protected(&params.Supabase))
 
 	//create a route
 	protected.Get("/user/:user_id", service.GetAllUserRatings)
