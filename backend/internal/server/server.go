d2cd90345cc2bd14b0dfc93cf1332091f4a5cbbb
jonafejzaj <fejzajjona@gmail.com>
2024-09-29 09:20:34 -0400

    adding crud routes to get venue ratings

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GenerateNU/nightlife/internal/handlers/hello"
 	"github.com/GenerateNU/nightlife/internal/handlers/test"
 	userrating "github.com/GenerateNU/nightlife/internal/handlers/user_ratings"
+	venueratings "github.com/GenerateNU/nightlife/internal/handlers/venue_ratings"
 	"github.com/GenerateNU/nightlife/internal/middleware"
 	"github.com/GenerateNU/nightlife/internal/types"
 	go_json "github.com/goccy/go-json"
@@ -28,6 +29,9 @@ func New(params types.Params) *fiber.App {
 
 	//User Ratings route group
 	userrating.Routes(app, params)
+
+	// Venue Ratings route group
+	venueratings.Routes(app, params)
 
 	return app
 }
--- backend/internal/storage/storage.go
+++ backend/internal/storage/storage.go
@@ -11,6 +11,7 @@ type Storage interface {
 	Close(context.Context) error
 	Test
 	UserRating
+	VenueRatings
 }
 
 type Test interface {
@@ -19,4 +20,8 @@ type Test interface {
 
 type UserRating interface {
 	GetAllUserRatings(context.Context, uuid.UUID) ([]models.UserRating, error)
+}
+
+type VenueRatings interface {
+	GetAllVenueRatings(context.Context, uuid.UUID) ([]models.VenueRatings, error)
 }
