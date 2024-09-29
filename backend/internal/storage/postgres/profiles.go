69c7b8e5e21d915ceace4f8d46ee514f0eddc107
kaamil2 <kaamil22thobani@gmail.com>
2024-09-29 19:56:46 -0400

    attempted lint

--- backend/internal/storage/postgres/profiles.go
+++ backend/internal/storage/postgres/profiles.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func (db *DB) UpdateProfilePrefences(ctx context.Context, user_id uuid.UUID, preference_type_to string, preference_value_to string, preferenceType string, preferenceValue string) error {
+func (db *DB) UpdateProfilePrefences(ctx context.Context, userID uuid.UUID, preferencetypeto string, preferencevalueto string, preferenceType string, preferenceValue string) error {
 
 	// SQL query execution
 	_, err := db.conn.Exec(ctx, `
@@ -18,14 +18,14 @@ func (db *DB) UpdateProfilePrefences(ctx context.Context, user_id uuid.UUID, pre
             preference_type = $2,
             preference_value = $3
         WHERE user_id = $1 AND preference_type = $4 AND preference_value = $5;
-    `, user_id, preference_type_to, preference_value_to, preferenceType, preferenceValue)
+    `, userID, preferencetypeto, preferencevalueto, preferenceType, preferenceValue)
 
 	if err != nil {
-		log.Printf("Failed to update preferences for userId %s: %v", user_id, err)
+		log.Printf("Failed to update preferences for userId %s: %v", userID, err)
 		return err
 	}
 
-	log.Printf("Successfully updated preferences for userId: %s", user_id)
+	log.Printf("Successfully updated preferences for userId: %s", userID)
 	return nil
 
 }
