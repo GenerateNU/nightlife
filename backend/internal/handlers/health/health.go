a4fd7d77f9527892648d57088bb9f9291f495053
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2024-09-30 11:15:19 -0400

    feat: health improvement (#41)

--- backend/internal/handlers/health/health.go
+++ backend/internal/handlers/health/health.go
@@ -1,22 +1,27 @@
 package health
 
 import (
-	"github.com/GenerateNU/nightlife/internal/types"
-	"github.com/gofiber/fiber/v2"
 	"net/http"
 	"runtime"
+
+	"github.com/GenerateNU/nightlife/internal/types"
+	"github.com/gofiber/fiber/v2"
 )
 
-func (s *Service) GetHealth(c *fiber.Ctx) error {
-	sysInfo := &types.SysInfo{
-		OS:           runtime.GOOS,
-		Architecture: runtime.GOARCH,
-		CPUCores:     runtime.NumCPU(),
-		GoVersion:    runtime.Version(),
-	}
+var health = &types.SysInfo{
+	OS:           runtime.GOOS,
+	Architecture: runtime.GOARCH,
+	CPUCores:     runtime.NumCPU(),
+	GoVersion:    runtime.Version(),
+}
 
-	return c.Status(http.StatusOK).JSON(fiber.Map{
-		"status": "ok",
-		"system": sysInfo,
-	})
+func (s *Service) GetHealth(c *fiber.Ctx) error {
+	return c.
+		Status(http.StatusOK).
+		JSON(
+			fiber.Map{
+				"status": "ok",
+				"system": health,
+			},
+		)
 }
