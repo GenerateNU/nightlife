80f3409c21f9c96bb80c91510750f5ab5a51833e
kaamil2 <kaamil22thobani@gmail.com>
2024-10-15 18:50:15 -0400

    merging conflicts

--- backend/internal/handlers/venues/venues.go
+++ backend/internal/handlers/venues/venues.go
@@ -1,7 +1,6 @@
 package venues
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -10,30 +9,18 @@ import (
 )
 
 func (s *Service) DeleteVenue(c *fiber.Ctx) error {
-	fmt.Println("Pinging correct service")
-
-	venueID := c.Params("venueId")
-	if venueID == "" {
-		c.Status(http.StatusBadRequest)
-		return fiber.NewError(400)
-	}
-	venueIDFormatted, err := uuid.Parse(venueID)
+	venueIDFormatted, err := uuid.Parse(c.Params("venueId"))
 
 	if err != nil {
 		c.Status(http.StatusBadRequest)
 		return fiber.NewError(400, "Delete failed due to malformed venue ID")
 	}
 
-	venueDelete := s.store.DeleteVenue(c.Context(), venueIDFormatted)
+	err = s.store.DeleteVenue(c.Context(), venueIDFormatted)
 
 	if err != nil {
-		fmt.Println("Error is on service line 26" + err.Error())
 		c.Status(http.StatusInternalServerError)
 		return err
-	}
-	if venueDelete != nil {
-		c.Status(http.StatusInternalServerError)
-		return fiber.NewError(500, "Delete failed")
 	}
 
 	return c.Status(http.StatusOK).JSON("deleted venue")
