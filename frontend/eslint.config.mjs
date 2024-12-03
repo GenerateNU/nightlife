d27bf8504ad87faedcc6df64838e40461bd9ee60
ihysi2024 <138328084+ihysi2024@users.noreply.github.com>
2024-12-03 11:35:43 -0500

    merge conflicts in backend endpoints resolved & images working

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,26 @@
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import { configs as tsConfigs } from "@typescript-eslint/eslint-plugin";
+import { configs as pluginReactConfigs } from "eslint-plugin-react";
 
 export default [
-  {ignores: ["**/*test*", "*babel.config.js*"],},
+  {
+    ignores: ["**/*test*", "*babel.config.js*"],
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    rules : {"react/react-in-jsx-scope" : "off",}
+  tsConfigs.recommended,
+  pluginReactConfigs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
   },
 ];
